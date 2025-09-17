1e4dd36a5d9b2b1023ded95719b0c59a83d69bc2
yuris0230 <178619823+yuris0230@users.noreply.github.com>
2025-09-17 22:30:05 +0000

    [Finish] 4-16, [Add] user update in controller, /views/users/edit & show page, /config/routes update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,21 @@ class UsersController < ApplicationController
     @post_images = @user.post_images
   end
 
+  # pull user from id in URL (/users/xxx/edit)
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   get "homes/about", to: "homes#about", as: "about"
 end
