6fec9002de70d2922da76f0778f917559ddb98a6
yuris0230 <178619823+yuris0230@users.noreply.github.com>
2025-09-17 21:24:02 +0000

    [Finish] 4-14, [Create] users controller, add users only show and edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   root to: "homes#top"
   devise_for :users
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
   get "homes/about", to: "homes#about", as: "about"
 end
