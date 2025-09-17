41afbda3db46c877b76e5624c51416c6680814db
yuris0230 <178619823+yuris0230@users.noreply.github.com>
2025-09-17 12:23:43 +0000

    [Finish] 4-9, [Create] PostImage's controller and [Add] some belongs to + has many + dependent destroy

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,3 +1,4 @@
 class PostImage < ApplicationRecord
   has_one_attached :image
+  belong_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "homes#top"
+  devise_for :users
+
+  resources :post_images, only: [:new, :index, :show]
   
   get "homes/about", to: "homes#about", as: "about"
 end
