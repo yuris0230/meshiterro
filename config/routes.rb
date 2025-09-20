bb078c07fabd210920451a6f8bb2d880f3e61d7d
yuris0230 <178619823+yuris0230@users.noreply.github.com>
2025-09-20 11:02:22 +0000

    [Finish 4-19], [Add] yarn barbel install for method(forgot), [Create] favo controller, favo models, [Fix] routing, views/post_images, model/post_image & user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  root to: "homes#top"
   devise_for :users
-  get "homes/about", to: "homes#about", as: "about"
+  root to: "homes#top"
+  get "/homes/about" => "homes#about", as: "about"
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resources :post_comments, only: [:create, :destroy]
+  resource :favorite, only: [:create, :destroy]
+  resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
 end
